959d2d504a5e825b7c1b88e6bbdfdf6a09c3dca0
shadslattery <shadslattery@gmail.com>
2019-12-19 10:17:05 -0500

    Trying to get catchup with everyone

--- app/src/main/java/com/example/kotlinfeud/model/Questions.kt
+++ app/src/main/java/com/example/kotlinfeud/model/Questions.kt
@@ -1,11 +1,40 @@
 package com.example.kotlinfeud.model
 
-class Questions (
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.kotlinfeud.R
+import java.time.Instant
+import java.util.zip.Inflater
+
+class Questions: Fragment() (
     var questionText: String,
     var answerA: String?,
     var answerB: String?,
     var answerC: String?,
     var answerD: String?,
-    var correctAns: String
+    var correctAns: String = ""
+
+
+private var questionIndex = 0
+// private val numQuestions = Math.min(((questionText.size + 1)) / 2,3)
+
+override fun onCreateView(inflater: LayoutInflater,container: ViewGroup),
+            savedInstant: Bundle?): View? {
+
+    val binding = DataBindingUtil.inflate<FragmentGameBinding>(
+            inflater, R.layout.question,container,false)
+
+    // Shuffles the questions and sets the question index to the first question
+    randomizeQuestions()
+
+    // Bind this fragment class to the layout
+    binding.Questions = this
+
+
+}
 
 )
