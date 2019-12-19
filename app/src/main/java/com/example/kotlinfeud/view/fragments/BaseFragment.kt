7230149493ec6266dc5fbe0b8e43d8f5e7da2179
Josh Dronyi <joshuadronyi@gmail.com>
2019-12-19 14:38:53 -0500

    Made a base fragment that holds the viewModel variable

---
+++ app/src/main/java/com/example/kotlinfeud/view/fragments/BaseFragment.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinfeud.view.fragments
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.example.kotlinfeud.view.activities.MainActivity
+import com.example.kotlinfeud.viewModel.MainViewModel
+
+class BaseFragment : Fragment() {
+
+    lateinit var viewModel: MainViewModel
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        viewModel = (context as MainActivity).viewModel
+    }
+}
