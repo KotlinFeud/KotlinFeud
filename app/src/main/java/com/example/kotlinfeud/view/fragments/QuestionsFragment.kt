071994782bf6933ad280d734fb282522fb948327
shadslattery <shadslattery@gmail.com>
2019-12-19 16:22:49 -0500

    @4:22 p made changes to my module

--- app/src/main/java/com/example/kotlinfeud/view/fragments/QuestionsFragment.kt
+++ app/src/main/java/com/example/kotlinfeud/view/fragments/QuestionsFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.kotlinfeud.R
+import kotlinx.android.synthetic.main.question.*
+import kotlinx.android.synthetic.main.question.view.*
 
-class QuestionsFragment : Fragment() {
+class QuestionsFragment : BaseFragment() {
 
     lateinit var correctAns: String
     private var questionIndex = 0
@@ -22,25 +23,39 @@ class QuestionsFragment : Fragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
 
-        var theView = inflater.inflate(R.layout.question,container,false)
+        var theView = inflater.inflate(R.layout.question, container, false)
+
+        val question =viewModel.startNewGame()
 
 
 
+        tv_question.text = question.questionText
+        rb_answer1.text = question.answerA
+        rb_answer2.text = question.answerB
+        rb_answer3.text = question.answerC
+        rb_answer4.text = question.answerD
 
+        btn_submit.setOnClickListener {
+            //This is what happens when the button gets clicked
 
+        }
 
-        // Bind this fragment class to the layout
 
         return theView
     }
 
+    // Bind this fragment class to the layout
+    fun onClickButton(v: View) {
+    }
+
+
     // Shuffles the questions and sets the question index to the first question
     //TODO("Set up the game to display the next question")
-    fun setUpGame(){
+    fun setUpGame() {
 
     }
 
-        fun checkAnswer(ans:String):Boolean{
-            return correctAns == ans
-        }
+    fun checkAnswer(ans: String): Boolean {
+        return correctAns == ans
+    }
 }
