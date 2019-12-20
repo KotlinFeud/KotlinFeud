ecb9a21d2cabece569eeaa3d067b18874dcf5982
sulaimanashiru <dash.ashiru@gmail.com>
2019-12-20 15:39:42 -0500

    added fragment and new layout/startgame_activity

---
+++ app/src/main/java/com/example/kotlinfeud/view/fragments/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinfeud.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment: BaseFragment () {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        val theView = inflater.inflate(R.layout.)
+    }
+}
